47b471a467a23a14f3706900409fe1bd6adf0654
air-2000 <41229917+air-2000@users.noreply.github.com>
2025-02-19 00:40:47 +0600

    Updated into classes for use in other programs

--- caesar.py
+++ caesar.py
@@ -1,30 +1,32 @@
-import string
+class CaesarCipher:
 
-ab = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-encrypt = ""
+    def encrypt(word, key, shift):
+        ab = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        encrypt = ""
+        word = word.upper()
+        if shift == "l":
+            for i in word:
+                if i in ab:
+                    #print(ab.index(i))
+                    #print((ab.index(i)+key)%25)
+                    encrypt = encrypt+ab[(ab.index(i)+key)%25]
+            return encrypt        
 
-word = input("Please input your word (preferably uppercase): ")
-key = int(input("Please input your key [INTEGER]: "))
+        elif shift == "r":
+            for i in word:
+                if i in ab:
+                    print(ab.index(i))
+                    #print((ab.index(i)-key)%25)
+                    encrypt = encrypt+ab[(ab.index(i)-key)%25]
+            return encrypt
 
-word = word.upper()
+        else:
+            return "Please input a proper response"
 
-shift = input("type l for left shift, r for right shift: ")
 
-if shift == "l":
-    for i in word:
-        if i in ab:
-            #print(ab.index(i))
-            #print((ab.index(i)+key)%25)
-            encrypt = encrypt+ab[(ab.index(i)+key)%25]
-    print(encrypt)        
 
-elif shift == "r":
-    for i in word:
-        if i in ab:
-            #print(ab.index(i))
-            #print((ab.index(i)-key)%25)
-            encrypt = encrypt+ab[(ab.index(i)-key)%25]
-    print(encrypt)
+#word = input("Please input your word (preferably uppercase): ")
+#key = int(input("Please input your key [INTEGER]: "))
+#shift = input("type l for left shift, r for right shift: ")
 
-else:
-    print("Please input a proper response")
+print(CaesarCipher.encrypt('Jame', 3, 'l'))
--- vignere.py
+++ vignere.py
@@ -1,16 +1,30 @@
-ab = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-encrypt = ""
-shift = 0
-word = input("Please input your word (preferably uppercase): ")
-key = input("Please input your key word: ")
+class VignereCipher:
+    #def __init__(self, word, key):
+    #    self.word = word.upper()
+    #    self.key = key.upper()
 
-word = word.upper()
-key = key.upper()
+    def encrypt(word, key):
+        ab = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        encrypt = ""
+        shift = 0
+        word = word.upper()
+        key = key.upper()
+        for i in word:
+            #print(i, key[word.index(i)%len(key)])
+            shift = (ab.index(i)+ab.index(key[word.index(i)%len(key)]))%25
+            #print(ab.index(i), ab.index(key[word.index(i)%len(key)]), shift)
+            encrypt = encrypt + ab[shift]
+        
+        return encrypt
+    
 
-for i in word:
-    print(i, key[word.index(i)%len(key)])
-    shift = (ab.index(i)+ab.index(key[word.index(i)%len(key)]))%25
-    print(ab.index(i), ab.index(key[word.index(i)%len(key)]), shift)
-    encrypt = encrypt + ab[shift]
 
-print(encrypt)
+#word = input("Please input your word (preferably uppercase): ")
+#key = input("Please input your key word: ")
+#word = word.upper()
+#key = key.upper()
+
+
+
+
+print(VignereCipher.encrypt('Jame','key'))
